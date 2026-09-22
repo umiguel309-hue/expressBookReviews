d13ffe09560541b0e6ce6b74ccdab571f3eaa7d4
Miguel <tu-correo@ejemplo.com>
2026-09-22 12:28:13 -0400

    Tarea 1 completada

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,9 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', function (req, res) {
+    res.send(JSON.stringify(books, null, 4));
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
