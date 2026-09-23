b0ea72ff2c1f7b0503bf0aa1fe22a499bfd24ef5
Miguel <tu-correo@ejemplo.com>
2026-09-23 11:37:32 -0400

    Tarea 2 completada

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,9 +17,9 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn], null, 4));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
