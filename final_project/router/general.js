8b1377a9c8cebc7658df9302ccc60bcfbe23d4c0
Miguel <tu-correo@ejemplo.com>
2026-09-23 11:47:52 -0400

    Tarea 3 completada

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,10 +23,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const author = req.params.author;
 
+  const matchingBooks = Object.keys(books).filter((isbn) => {
+    return books[isbn].author === author;
+  });
+
+  const result = matchingBooks.map((isbn) => books[isbn]);
+
+  res.send(JSON.stringify(result, null, 4));
+});
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
