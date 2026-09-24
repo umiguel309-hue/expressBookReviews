63722c11a66d6b175afbbcb714cb201c930022af
Miguel <umiguel309@gmail.com>
2026-09-24 11:46:41 -0400

    Tarea 7 completada

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,18 +6,34 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+    let userswithsamename = users.filter((user) => user.username === username);
+    return userswithsamename.length > 0;
+  }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+  const authenticatedUser = (username,password)=>{ //returns boolean
+    let validusers = users.filter((user) => user.username === username && user.password === password);
+    return validusers.length > 0;
+  }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  regd_users.post("/login", (req,res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      return res.status(404).json({message: "Usuario y contraseña son obligatorios"});
+    }
+  
+    if (!authenticatedUser(username, password)) {
+      return res.status(208).json({message: "Usuario o contraseña incorrectos"});
+    }
+  
+    let accessToken = jwt.sign({ username: username }, 'access', { expiresIn: 60 * 60 });
+  
+    req.session.authorization = {
+      accessToken, username
+    };
+  
+    return res.status(200).json({message: "Usuario logueado exitosamente"});
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
